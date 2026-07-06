3dae56e2653d8f5d4d5fe3aa371ee010d3ce7711
adinislamova <adinislamova@cloudfactory.ru>
2026-07-06 13:54:53 +0300

    Добавление поиска по zip и постраничного вывода

--- ex7/actions.py
+++ ex7/actions.py
@@ -1,4 +1,4 @@
-##действия с рынками
+## действия с рынками
 
 ## поиск по городу
 def find_by_city(markets, city):
@@ -19,3 +19,22 @@ def find_by_state(markets, state):
             result.append(market)
 
     return result
+
+## поиск по zip
+def find_by_zip(markets, zip_code):
+    result = []
+
+    for market in markets:
+        if market["zip"] == zip_code:
+            result.append(market)
+
+    return result
+
+## показывать постранично
+def show_page(markets, page, page_size):
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    page_markets = markets[start:end]
+
+    return page_markets
--- ex7/main.py
+++ ex7/main.py
@@ -1,23 +1,77 @@
 ## основа
 
 from data import load_markets
-from actions import find_by_city, find_by_state
+from actions import find_by_city, find_by_state, find_by_zip, show_page
 
 markets = load_markets("Export.csv")
 
 while True:
 
-    choice = input("По чему ищем? город/штат: ") ##добавляем выбор для поиска
+    print()
+    print("Что хотите сделать?")
+    print("1 - Показать рынки")
+    print("2 - Найти по городу")
+    print("3 - Найти по штату")
+    print ("4 - Найти по ZIP-коду")
+    print("0 - Выход")
 
-    if choice == "город":
+    choice = input("Ваш выбор: ") ##добавляем выбор для поиска
+
+    if choice == "1":
+        page = int(input("Введите страницу: ")) ##спрашиваем страницу
+        result = show_page(markets, page, 10) ##ищем рынки постранично, реализовано в actions
+
+    elif choice == "2":
         city = input("Введите город: ") ##спрашиваем город
         result = find_by_city(markets, city) ##ищем рынки по городу, реализовано в actions
 
-    elif choice == "штат":
-        state = input("Введите штат: ")
-        result = find_by_state(markets, state)
+    elif choice == "3":
+        state = input("Введите штат: ") ## спрашиваем штат
+        result = find_by_state(markets, state) ## реализовано в actions
+
+    elif choice == "4":
+        zip_code = input("Введите ZIP-код: ")## спрашиваем zip
+        result = find_by_zip(markets, zip_code)
+
+        if not zip_code:
+            print("ZIP-код не может быть пустым")
+            continue
+        
+        if not result:
+            print("Ничего не найдено.")
+            continue
+
+    elif choice == "0":
+        print ("До свидания!") ## выход
+        break
 
     else:
-        result = []
+        print ("Неверный выбор") ## выход
+        continue
     
-    print(result) ##выводим результат
+    for number, market in enumerate(result, start=1): ##выводим результат, добавила enumerate, для нумерации всех маркетов
+        print("-" * 40)
+        print(number)
+        print("Название:", market["name"])
+        print("Город:", market["city"])
+        print("Штат:", market["state"])
+
+    number = int(input("Введите номер рынка (0 - назад): "))
+
+    if number == 0:
+        continue
+
+    if number < 1 or number > len(result):
+        print("Такого номера нет.")
+        continue
+
+    selected = result[number - 1]
+
+    print()
+    print("Подробная информация:")
+    print("Название:", selected["name"])
+    print("Город:", selected["city"])
+    print("Штат:", selected["state"])
+    print("Индекс:", selected["zip"])
+    print("Широта:", selected["lat"])
+    print("Долгота:", selected["lon"])
